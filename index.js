e7da13a3ae51947f71152875ac6565979073f170
Alice Lipski <alipski@pop-os.localdomain>
2022-09-02 20:43:48 -0300

    feat(POST/participants): input validation with joi library implemented [string, required and not empty]

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 import express from "express";
 import { MongoClient } from "mongodb";
+import dotenv from 'dotenv';
+import joi from 'joi';
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-// conectando ao banco
-const mongoClient = new MongoClient("mongodb://localhost:27017");
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
-mongoClient.connect().then(() => {
-	db = mongoClient.db("meu_banco_de_dados");
+mongoClient.connect(() => {
+  db = mongoClient.db("batepapo-uol");
 });
 
-app.get("/usuarios", (req, res) => {
-	// buscando usuários
-	db.collection("users").find().toArray().then(users => {
-		console.log(users); // array de usuários
-	});
+app.post("/participants", async (req, res) => {
+	const name = req.body.name;
+
+	const nameSchema = joi.object({
+		name: joi.string().required()
+	  });
+
+	const validation = nameSchema.validate({ name: name }, { abortEarly: true });
+	if (validation.error) {
+		res.sendStatus(422);
+		return;
+	  }
+
+	res.send('Created ' + name);
 });
 
-app.post("/usuarios", (req, res) => {
-	// inserindo usuário
-	db.collection("users").insertOne({
-		email: "joao@email.com",
-		password: "minha_super_senha"
-	});
-});
-
-app.listen(5000);
+app.listen(5000, () => {
+	console.log('Server is listening on port 5000.');
+  });
